a9a439fabd90e742f055dc2522d63e9600cd1d55
Ellensi Rey Chandra <ellensi.chandra@autodesk.com>
2015-09-29 15:26:50 +0800

    Cleanup layout inside groups

--- src/DynamoCoreWpf/Properties/Resources.Designer.cs
+++ src/DynamoCoreWpf/Properties/Resources.Designer.cs
@@ -2164,6 +2164,15 @@ namespace Dynamo.Wpf.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Cleanup Layout.
+        /// </summary>
+        public static string GroupContextMenuGraphLayout {
+            get {
+                return ResourceManager.GetString("GroupContextMenuGraphLayout", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Ungroup.
         /// </summary>
         public static string GroupContextMenuUngroup {
